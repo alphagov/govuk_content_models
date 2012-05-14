896cc7479af5174fccd4ed196cd5f0d053e2583b
Paul Battley <pbattley@gmail.com>
2012-05-14 14:03:40 +0100

    Remove superseded hack.

--- app/models/artefact.rb
+++ app/models/artefact.rb
@@ -53,10 +53,6 @@ class Artefact
 
   before_save :save_section_as_tags
 
-  # TODO: Remove this 'unless' hack after importing. It's only here because
-  # some old entries in Panopticon lack a need_id.
-  validates_presence_of :need_id, :unless => lambda { defined? IMPORTING_LEGACY_DATA }
-
   def self.in_alphabetical_order
     order_by([[:name, :asc]])
   end
