41de7a25b7891c8235431cfd999124988853091b
Kushal Pisavadia <kushi.p@gmail.com>
2012-05-17 02:23:45 -0700

    Add test for Tag when showing as_json
    The basic unit of TagRepository now has tests wrapping it.

---
+++ test/models/tag_test.rb
@@ -0,0 +1,9 @@
+require "test_helper"
+require "tag"
+
+class TagTest < ActiveSupport::TestCase
+  test "should return a hash of the fields" do
+    tag = Tag.new(:tag_id => "crime", :tag_type => "section", :title => "Crime")
+    assert_equal tag.as_json, {:id=>"crime", :title=>"Crime", :type=>"section"}
+  end
+end
