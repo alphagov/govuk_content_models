4b13bf58c4c1bec7ed9cc22d4cc6931312d4f682
James Stewart <jys@ketlai.co.uk>
2012-08-10 14:00:04 +0100

    Update test for JSON form of tag
    This should have included the description. Now passes. Also slightly reformatted for ruby 1.9 hash syntax

--- test/models/tag_test.rb
+++ test/models/tag_test.rb
@@ -3,7 +3,11 @@ require "tag"
 
 class TagTest < ActiveSupport::TestCase
   test "should return a hash of the fields" do
-    tag = Tag.new(:tag_id => "crime", :tag_type => "section", :title => "Crime")
-    assert_equal tag.as_json, {:id=>"crime", :title=>"Crime", :type=>"section"}
+    tag = Tag.new(
+      tag_id: "crime",
+      tag_type: "section",
+      title: "Crime"
+    )
+    assert_equal tag.as_json, {id: "crime", title: "Crime", type: "section", description: nil}
   end
 end
