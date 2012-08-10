cfb2a3cf43896579e9cd03acb648ede4bd7ae92a
Kushal Pisavadia <kushi.p@gmail.com>
2012-08-10 14:19:00 +0000

    Remove tests which are only checking field read/writes
    As made clear to me by @mnowster, having tests for String fields
    is pointless and makes it harder later on to change our schema.
    
    Field tests like this should only be done if we have callbacks,
    observers, or validations which are affected by changing such
    fields.

--- test/models/video_edition_test.rb
+++ test/models/video_edition_test.rb
@@ -11,20 +11,6 @@ class VideoEditionTest < ActiveSupport::TestCase
     assert_equal "Video", video.format
   end
 
-  test "should allow saving a video URL" do
-    video = VideoEdition.new
-    assert_equal nil, video.video_url
-    video.video_url = "http://www.youtube.com/watch?v=tDkVS-AN4NU"
-    assert_equal "http://www.youtube.com/watch?v=tDkVS-AN4NU", video.video_url
-  end
-
-  test "should allow saving a video summary" do
-    video = VideoEdition.new
-    assert_equal nil, video.video_summary
-    video.video_summary = "This is a parody of Wonders of the Universe"
-    assert_equal "This is a parody of Wonders of the Universe", video.video_summary
-  end
-
   test "should have a whole_body method which is nil if video URL and video summary aren't present" do
     video = VideoEdition.new
     assert_equal nil, video.whole_body
