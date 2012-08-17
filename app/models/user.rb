cc0f44a003fdd61f94c8a48768c5d23e143382b4
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-08-17 14:18:30 +0000

    Apps should store their users separately

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,6 +9,11 @@ class User
   include GDS::SSO::User
   include WorkflowActor
 
+  def self.application_name
+    Rails.application.class.parent_name.downcase
+  end
+  self.collection_name = "#{application_name}_users"
+
   field "name",                type: String
   field "uid",                 type: String
   field "version",             type: Integer
