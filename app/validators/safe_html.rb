b508f9b2b1eb4d034c56da5fc472b2f565e51af8
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-08-22 13:47:31 +0000

    Rename method

--- app/validators/safe_html.rb
+++ app/validators/safe_html.rb
@@ -8,36 +8,35 @@ class SafeHtml < ActiveModel::Validator
     end
   end
 
-  private
-    def check_struct(record, field_name, value)
-      if value.respond_to?(:values) # e.g. Hash
-        value.values.each { |entry| check_struct(record, field_name, entry) }
-      elsif value.respond_to?(:each) # e.g. Array
-        value.each { |entry| check_struct(record, field_name, entry) }
-      elsif value.is_a?(String)
-        check_field(record, field_name, value)
-      end
+  def check_struct(record, field_name, value)
+    if value.respond_to?(:values) # e.g. Hash
+      value.values.each { |entry| check_struct(record, field_name, entry) }
+    elsif value.respond_to?(:each) # e.g. Array
+      value.each { |entry| check_struct(record, field_name, entry) }
+    elsif value.is_a?(String)
+      check_string(record, field_name, value)
     end
+  end
 
-    def check_field(record, field_name, new_value)
-      dirty_html = Govspeak::Document.new(new_value).to_html
-      dirty_html.strip!
-      clean_html = Sanitize.clean(dirty_html, sanitize_config)
-      # Trying to make whitespace consistent
-      if Nokogiri::HTML.parse(dirty_html).to_s != Nokogiri::HTML.parse(clean_html).to_s
-        record.errors.add(field_name, "Invalid Govspeak or JavaScript is not allowed in #{field_name}")
-      end
+  def check_string(record, field_name, new_value)
+    dirty_html = Govspeak::Document.new(new_value).to_html
+    dirty_html.strip!
+    clean_html = Sanitize.clean(dirty_html, sanitize_config)
+    # Trying to make whitespace consistent
+    if Nokogiri::HTML.parse(dirty_html).to_s != Nokogiri::HTML.parse(clean_html).to_s
+      record.errors.add(field_name, "Invalid Govspeak or JavaScript is not allowed in #{field_name}")
     end
+  end
 
-    def sanitize_config
-      config = Sanitize::Config::RELAXED.dup 
-      
-      config[:attributes][:all] << "id"
-      config[:attributes][:all] << "class"
-      config[:attributes]["a"]  << "rel"
+  def sanitize_config
+    config = Sanitize::Config::RELAXED.dup 
+    
+    config[:attributes][:all] << "id"
+    config[:attributes][:all] << "class"
+    config[:attributes]["a"]  << "rel"
 
-      config[:elements] << "div"
+    config[:elements] << "div"
 
-      config
-    end
+    config
+  end
 end
