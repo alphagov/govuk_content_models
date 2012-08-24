c1c108633739bb876e99a38ca3a04d9d63256caf
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2012-08-24 13:09:26 +0000

    Make usage in a non-Rails app easier

---
+++ lib/govuk_content_models/require_all.rb
@@ -0,0 +1,11 @@
+# Require this file in a non-Rails app to load all the things
+
+%w[ app/models app/validators app/repositories app/traits lib ].each do |path|
+  full_path = File.expand_path("../../../#{path}", __FILE__)
+  $LOAD_PATH.unshift full_path unless $LOAD_PATH.include?(full_path)
+end
+
+# Require everything under app
+Dir.glob("#{File.dirname(__FILE__)}/../../app/**/*.rb").each do |file|
+  require file
+end
