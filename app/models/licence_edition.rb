b76379a117bc63e0154536500c2ca6b7a83e6d32
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2012-09-06 16:17:25 +0000

    Added continuation fields to LicenceEdition

--- app/models/licence_edition.rb
+++ app/models/licence_edition.rb
@@ -5,12 +5,15 @@ class LicenceEdition < Edition
   field :licence_identifier,  :type => String
   field :licence_short_description, :type => String
   field :licence_overview,  :type => String
+  field :will_continue_on, :type => String
+  field :continuation_link, :type => String
 
   validates :licence_identifier, :presence => true
   validate :licence_identifier_unique
-
+  validates_format_of :continuation_link, :with => URI::regexp(%w(http https)), :allow_blank => true
+  
   @fields_to_clone = [:licence_identifier, :licence_short_description,
-                      :licence_overview]
+                      :licence_overview, :will_continue_on, :continuation_link]
 
   def whole_body
     [licence_short_description, licence_overview].join("\n\n")
--- test/models/licence_edition_test.rb
+++ test/models/licence_edition_test.rb
@@ -10,12 +10,16 @@ class LicenceEditionTest < ActiveSupport::TestCase
     l.licence_identifier = "AB1234"
     l.licence_short_description = "Short description of licence"
     l.licence_overview = "Markdown overview of licence..."
+    l.will_continue_on = "The HMRC website"
+    l.continuation_link = "http://www.hmrc.gov.uk"
     l.safely.save!
 
     l = LicenceEdition.first
     assert_equal "AB1234", l.licence_identifier
     assert_equal "Short description of licence", l.licence_short_description
     assert_equal "Markdown overview of licence...", l.licence_overview
+    assert_equal "The HMRC website", l.will_continue_on
+    assert_equal "http://www.hmrc.gov.uk", l.continuation_link
   end
 
   context "validations" do
@@ -44,6 +48,15 @@ class LicenceEditionTest < ActiveSupport::TestCase
       assert_equal 'wibble', new_version.licence_identifier
       assert new_version.valid?, "Expected clone to be valid"
     end
+    
+    should "validate the continuation link if not blank" do
+      @l.continuation_link = ""
+      assert @l.valid?, "continuation_link validation should not be triggered when the field is blank"
+      @l.continuation_link = "not&a+valid_url"
+      assert !@l.valid?, "continuation_link validation should fail with a invalid url"
+      @l.continuation_link = "http://www.hmrc.gov.uk"
+      assert @l.valid?, "continuation_link validation should pass with a valid url"
+    end
   end
 
   should "clone extra fields when cloning edition" do
@@ -52,11 +65,15 @@ class LicenceEditionTest < ActiveSupport::TestCase
                                  :state => "published",
                                  :licence_identifier => "1234",
                                  :licence_short_description => "Short description of licence",
-                                 :licence_overview => "Overview to be cloned")
+                                 :licence_overview => "Overview to be cloned",
+                                 :will_continue_on => "Continuation text to be cloned",
+                                 :continuation_link => "http://www.gov.uk")
     new_licence = licence.build_clone
 
     assert_equal licence.licence_identifier, new_licence.licence_identifier
     assert_equal licence.licence_short_description, new_licence.licence_short_description
     assert_equal licence.licence_overview, new_licence.licence_overview
+    assert_equal licence.will_continue_on, new_licence.will_continue_on
+    assert_equal licence.continuation_link, new_licence.continuation_link
   end
 end
