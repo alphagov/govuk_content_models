28e9dc67a2ece91cc816539fe2e290aa809c57e2
Alex Tomlins <alex.tomlins@digital.cabinet-office.gov.uk>
2012-11-08 14:49:38 +0000

    Refine which fields are govspeak
    This enables the content_api to govspeak them appropriately.

--- app/models/completed_transaction_edition.rb
+++ app/models/completed_transaction_edition.rb
@@ -4,6 +4,8 @@ require "safe_html"
 class CompletedTransactionEdition < Edition
   field :body, type: String
 
+  GOVSPEAK_FIELDS = Edition::GOVSPEAK_FIELDS + [:body]
+
   @fields_to_clone = [:body]
 
   def whole_body
