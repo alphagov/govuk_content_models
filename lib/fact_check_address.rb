7aab0f0774ba94a71f3c03e032d2a527308cefca
James Stewart <jys@ketlai.co.uk>
2012-11-30 13:24:42 +0000

    Replace FactCheckAddress#regexp with FactCheckAddress#valid_address?

--- lib/fact_check_address.rb
+++ lib/fact_check_address.rb
@@ -7,11 +7,15 @@ class FactCheckAddress
     "#{prefix}#{edition.id}@#{DOMAIN}"
   end
 
+  def valid_address?(address)
+    regexp.match(address)
+  end
+
+  private
   def regexp
     /#{prefix}(.+?)@#{DOMAIN}/
   end
 
-  private
   def prefix
     "factcheck+#{environment}-"
   end
