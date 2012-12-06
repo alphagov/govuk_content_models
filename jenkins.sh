a6840a4e7af5c48a6501f2618f5633cb89599063
Mazz Mosley <mazz.mosley@digital.cabinet-office.gov.uk>
2012-12-06 15:32:48 +0000

    Include govuk_setenv to allow us to build the gem

--- jenkins.sh
+++ jenkins.sh
@@ -2,5 +2,5 @@
 set -e
 rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
-bundle exec rake test
-bundle exec rake publish_gem
+govuk_setenv default bundle exec rake test
+govuk_setenv default bundle exec rake publish_gem
