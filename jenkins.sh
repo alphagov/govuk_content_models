1f02509b8d01b64bd9453126e33b3cfc613dce4c
Mazz Mosley <mazz.mosley@digital.cabinet-office.gov.uk>
2012-12-06 15:41:19 +0000

    Remove govuk_setenv as we're setting it manually

--- jenkins.sh
+++ jenkins.sh
@@ -3,5 +3,5 @@ set -e
 rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 export GOVUK_APP_DOMAIN=development
-govuk_setenv default bundle exec rake test
-govuk_setenv default bundle exec rake publish_gem
+bundle exec rake test
+bundle exec rake publish_gem
