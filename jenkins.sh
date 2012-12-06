6081a4a3bbab044bd9998b3a1fdbcd9fa4fb10bf
Mazz Mosley <mazz.mosley@digital.cabinet-office.gov.uk>
2012-12-06 15:38:48 +0000

    Manually set GOVUK_APP_DOMAIN
    Due to jenkins being on EC2 still and the puppet master being disabled on there, it won't have this env var set.

--- jenkins.sh
+++ jenkins.sh
@@ -2,5 +2,6 @@
 set -e
 rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
+export GOVUK_APP_DOMAIN=development
 govuk_setenv default bundle exec rake test
 govuk_setenv default bundle exec rake publish_gem
