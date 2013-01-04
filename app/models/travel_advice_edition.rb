60390e56fb9e500ce564feeef99b991af698c6ab
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-01-04 15:00:09 +0000

    Add validation of state for country slug
    Only allow one TravelAdviceEdition in draft or published state per country slug.

--- app/models/travel_advice_edition.rb
+++ app/models/travel_advice_edition.rb
@@ -5,11 +5,10 @@ class TravelAdviceEdition
 
   field :country_slug,         type: String
   field :version_number,       type: Integer,   default: 1
-  field :sibling_in_progress,  type: Integer,   default: nil
   field :state,                type: String,    default: "draft"
 
   validates_presence_of :country_slug
-  validates_uniqueness_of :country_slug
+  validate :state_for_slug_unique
 
   state_machine initial: :draft do
     event :publish do
@@ -18,6 +17,17 @@ class TravelAdviceEdition
 
     event :archive do
       transition all => :archived, :unless => :archived?
+    end
+  end
+
+  private
+
+  def state_for_slug_unique
+    existing = self.class.where(:state.ne => "archived",
+                                :state => state,
+                                :country_slug => country_slug)
+    if existing.any? and existing != [self]
+      errors.add(:state, :taken)
     end
   end
 
--- test/models/travel_advice_edition_test.rb
+++ test/models/travel_advice_edition_test.rb
@@ -11,11 +11,16 @@ class TravelAdviceEditionTest < ActiveSupport::TestCase
     assert_includes ta.errors.messages[:country_slug], "can't be blank" 
   end
 
-  test "country slug must be unique" do
+  test "country slug must be unique for state" do
     @ta.save!
-    another_edition = TravelAdviceEdition.new(:country_slug => "narnia")
+    another_edition = FactoryGirl.create(:travel_advice_edition, 
+                                         :country_slug => "liliputt", 
+                                         :state => "draft")
+    another_edition.country_slug = "narnia"
     another_edition.valid?
-    assert_includes another_edition.errors.messages[:country_slug], "is already taken"
+    assert_includes another_edition.errors.messages[:state], "is already taken"
+    another_edition.publish!
+    assert another_edition.valid?
   end
 
   test "a new travel advice edition is a draft" do
