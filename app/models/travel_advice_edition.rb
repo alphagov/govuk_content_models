8371f25d70504d6d64004482a8c5da8d623b81da
Steve Laing <steve.laing@digital.cabinet-office.gov.uk>
2013-01-04 12:24:17 +0000

    Add country_slug validations

--- app/models/travel_advice_edition.rb
+++ app/models/travel_advice_edition.rb
@@ -1,13 +1,15 @@
-
 class TravelAdviceEdition
   include Mongoid::Document
   include Mongoid::Timestamps
   include Parted
 
-  field :country_slug, type: String
-  field :version_number,       type: Integer,  default: 1
-  field :sibling_in_progress,  type: Integer,  default: nil
-  field :state, type: String, default: "draft"
+  field :country_slug,         type: String
+  field :version_number,       type: Integer,   default: 1
+  field :sibling_in_progress,  type: Integer,   default: nil
+  field :state,                type: String,    default: "draft"
+
+  validates_presence_of :country_slug
+  validates_uniqueness_of :country_slug
 
   state_machine initial: :draft do
     event :publish do
--- test/models/travel_advice_edition_test.rb
+++ test/models/travel_advice_edition_test.rb
@@ -2,7 +2,20 @@ require "test_helper"
 
 class TravelAdviceEditionTest < ActiveSupport::TestCase
   setup do
-    @ta = TravelAdviceEdition.new
+    @ta = TravelAdviceEdition.new(:country_slug => "narnia")
+  end
+
+  test "country slug must be present" do
+    ta = TravelAdviceEdition.new
+    ta.valid?
+    assert_includes ta.errors.messages[:country_slug], "can't be blank" 
+  end
+
+  test "country slug must be unique" do
+    @ta.save!
+    another_edition = TravelAdviceEdition.new(:country_slug => "narnia")
+    another_edition.valid?
+    assert_includes another_edition.errors.messages[:country_slug], "is already taken"
   end
 
   test "a new travel advice edition is a draft" do
