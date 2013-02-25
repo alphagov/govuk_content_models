e623b3c87fd4f6447adc5d67fae6614a0d66d5c1
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-02-25 16:53:29 +0000

    Refactor for readability

--- app/validators/slug_validator.rb
+++ app/validators/slug_validator.rb
@@ -1,17 +1,16 @@
 class SlugValidator < ActiveModel::EachValidator
   # implement the method called during validation
   def validate_each(record, attribute, value)
-    # allow slugs to have the done/ prefix
     if value.to_s =~ /^done\/(.+)/
-      value = $1
-    # allow foreign-travel-advice prefix
+      last_part = $1
     elsif value.to_s =~ /\Aforeign-travel-advice\/(.+)/ and record.kind == 'travel-advice'
-      value = $1
+      last_part = $1
     elsif value.to_s =~ /\Agovernment\/(.+)/ and record.kind == 'inside_government'
-      value = $1
+      last_part = $1
+    else
+      last_part = value
     end
-
-    unless ActiveSupport::Inflector.parameterize(value.to_s) == value.to_s
+    unless ActiveSupport::Inflector.parameterize(last_part.to_s) == last_part.to_s
       record.errors[attribute] << "must be usable in a URL"
     end
   end
