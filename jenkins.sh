4f254c1ccb59c3a43f3332717698d0b5c27f5e64
Alex Tomlins <alex@tomlins.org.uk>
2014-02-06 16:30:20 +0000

    Revert "Only publish gem on master branch"
    This reverts commit 4ac412a0879e0b4ada2f6827dd1893d6c3a532e9.

--- jenkins.sh
+++ jenkins.sh
@@ -4,6 +4,4 @@ rm -f Gemfile.lock
 bundle install --path "${HOME}/bundles/${JOB_NAME}"
 export GOVUK_APP_DOMAIN=dev.gov.uk
 bundle exec rake
-if [ "${GIT_BRANCH}" == "master" ]; then
-  bundle exec rake publish_gem
-fi
+bundle exec rake publish_gem
