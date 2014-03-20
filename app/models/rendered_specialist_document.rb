7fb1d3d0fd0fa7e84c5f2077f7474a8c63c81262
David Heath <david.heath@digital.cabinet-office.gov.uk>
2014-03-20 16:43:10 +0000

    Increase indentation

--- app/models/rendered_specialist_document.rb
+++ app/models/rendered_specialist_document.rb
@@ -2,17 +2,17 @@ class RenderedSpecialistDocument
   include Mongoid::Document
   include Mongoid::Timestamps
 
-  field :slug,          type: String
-  field :title,         type: String
-  field :summary,       type: String
-  field :body,          type: String
-  field :opened_date,   type: Date
-  field :closed_date,   type: Date
-  field :case_type,     type: String
-  field :case_state,    type: String
-  field :market_sector, type: String
-  field :outcome_type,  type: String
-  field :headers,       type: Array
+  field :slug,                   type: String
+  field :title,                  type: String
+  field :summary,                type: String
+  field :body,                   type: String
+  field :opened_date,            type: Date
+  field :closed_date,            type: Date
+  field :case_type,              type: String
+  field :case_state,             type: String
+  field :market_sector,          type: String
+  field :outcome_type,           type: String
+  field :headers,                type: Array
 
   index "slug", unique: true
 
