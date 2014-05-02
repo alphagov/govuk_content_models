e3d611a82f623df5f13fa249f22c80cae143e3c2
Dominic Baggott <dominic.baggott@gmail.com>
2014-05-02 14:53:46 +0100

    Add ManualEdition

---
+++ app/models/manual_edition.rb
@@ -0,0 +1,13 @@
+class ManualEdition
+  include Mongoid::Document
+  include Mongoid::Timestamps
+
+  field :manual_id, type: String
+  field :version_number, type: Integer, default: 1
+
+  field :title, type: String
+  field :created_at, type: DateTime, default: lambda { Time.zone.now }
+  field :state, type: String, default: 'draft'
+
+  field :summary, type: String
+end
