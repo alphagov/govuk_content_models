53d120dc28856d8ee431c543a608eff1c6d3e082
Dominic Baggott <dominic.baggott@gmail.com>
2014-05-30 15:36:59 +0100

    Add manual format

--- app/models/artefact.rb
+++ app/models/artefact.rb
@@ -82,7 +82,9 @@ class Artefact
     "smartanswers"            => ["smart-answer"],
     "custom-application"      => ["custom-application"], # In this case the owning_app is overriden. eg calendars, licencefinder
     "travel-advice-publisher" => ["travel-advice"],
-    "specialist-publisher"    => ["specialist-document"],
+    "specialist-publisher"    => ["manual",
+                                  "manual-section",
+                                  "specialist-document"],
     "finder-api"              => ["finder"],
     "whitehall"               => ["announcement",
                                   "authored_article",
