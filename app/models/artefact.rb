1c086e9b7225b7c7f6a124867a85f905c668e78e
Dominic Baggott <dominic.baggott@gmail.com>
2014-06-11 09:23:26 +0100

    Add manual-change-history artefact and validation
    A manual-change-history artefact will contain change history for a
    manual artefact. The app which publishes the manual artefact will create
    and update the manual-change-history artefact as necessary.

--- app/models/artefact.rb
+++ app/models/artefact.rb
@@ -83,6 +83,7 @@ class Artefact
     "custom-application"      => ["custom-application"], # In this case the owning_app is overriden. eg calendars, licencefinder
     "travel-advice-publisher" => ["travel-advice"],
     "specialist-publisher"    => ["manual",
+                                  "manual-change-history",
                                   "manual-section",
                                   "specialist-document"],
     "finder-api"              => ["finder"],
