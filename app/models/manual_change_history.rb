dcb81a18131a698fdca58f1654572f63d0dc8e8d
Stephen Best <bestie@gmail.com>
2014-06-11 12:00:48 +0100

    Add ManualChangeHistory model

---
+++ app/models/manual_change_history.rb
@@ -0,0 +1,18 @@
+require "prerendered_entity"
+
+class ManualChangeHistory
+  include Mongoid::Document
+  include Mongoid::Timestamps
+  extend PrerenderedEntity
+
+  field :updates, type: Array
+  field :slug, type: String
+  field :manual_slug, type: String
+
+  index "slug", unique: true
+
+  validates :slug, uniqueness: true
+  validates_with SafeHtml
+
+  GOVSPEAK_FIELDS = []
+end
