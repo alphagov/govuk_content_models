e46b648a20b8b3b46290330a9dc626838dfdcc7b
Vinay Patel <vinay.patel@digital.cabinet-office.gov.uk>
2014-06-24 09:26:33 +0000

    Validate links on any attribute changes
    earlier we validates govspeak fields in an
    edition only when they changed. instead, we
    should validate and present all errors in
    the document on every save.

--- test/validators/link_validator_test.rb
+++ test/validators/link_validator_test.rb
@@ -5,6 +5,7 @@ class LinkValidatorTest < ActiveSupport::TestCase
     include Mongoid::Document
 
     field "body", type: String
+    field "assignee", type: String
     GOVSPEAK_FIELDS = [:body]
 
     validates_with LinkValidator
@@ -22,24 +23,39 @@ class LinkValidatorTest < ActiveSupport::TestCase
       doc = Dummy.new(body: "abc [internal](/internal)")
       assert doc.valid?
     end
+
     should "not contain hover text" do
       doc = Dummy.new(body: 'abc [foobar](http://foobar.com "hover")')
       assert doc.invalid?
       assert_includes doc.errors.keys, :body
     end
+
     should "not set rel=external" do
       doc = Dummy.new(body: 'abc [foobar](http://foobar.com){:rel="external"}')
       assert doc.invalid?
       assert_includes doc.errors.keys, :body
     end
+
     should "show multiple errors" do
       doc = Dummy.new(body: 'abc [foobar](foobar.com "bar"){:rel="external"}')
       assert doc.invalid?
       assert_equal 3, doc.errors[:body].first.length
     end
+
     should "only show each error once" do
       doc = Dummy.new(body: 'abc [link1](foobar.com), ghi [link2](bazquux.com)')
       assert doc.invalid?
+      assert_equal 1, doc.errors[:body].first.length
+    end
+
+    should "be validated when any attribute of the document changes" do
+      # already published document having link validation errors
+      doc = Dummy.new(body: 'abc [link1](foobar.com), ghi [link2](bazquux.com)')
+      doc.save(validate: false)
+
+      doc.assignee = "4fdef0000000000000000001"
+      assert doc.invalid?
+
       assert_equal 1, doc.errors[:body].first.length
     end
   end
